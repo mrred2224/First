60261f9d2d5d6c41160d8caba3df18aa43429f98
mrred2224 <sakthi.kveni@gmail.com>
2021-03-17 23:52:49 +0530

    Mine

---
+++ src/test/java/org/test/Alpha.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Alpha {
+public static void main(String[] args) throws FileNotFoundException {
+	File f = new File("C:\\Users\\91638\\eclipse-workspace\\FirstMaveenPro\\Excel\\FileExample.xlsx");
+FileInputStream fs = new FileInputStream(f);
+workbook
+
+
+
+}
+}
